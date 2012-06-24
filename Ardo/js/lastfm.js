531ad07381d658aac842474273d9a1c36ddd4a25
Benson Tsai <btsai@vrwarp.com>
2012-06-23 23:47:33 -0700

    chris ruining everything

--- Ardo/js/lastfm.js
+++ Ardo/js/lastfm.js
@@ -4,35 +4,24 @@
     var key = "b25b959554ed76058ac220b7b2e0a026";
 
     var getSimilarArtists = function (artist, callback, error) {
-        //app.lastfm.artist.getSimilar({
-        //    artist: artist
-        //},
-        //{
-        //    success: function (data) {
-        //        var artists = data.similarartists.artist;
-        //        for (var i = 1; i < artists.length; i++) {
-        //            if (artists[i].image[1]['#text'] != "") app.setupImage(artists[i]);
-        //        }
-        //        $('#artists').fadeIn();
-        //        loadingOff();
-        //    },
-        //    error: function (code, message) {
-        //        alert('Unable to retrieve data from last.fm');
-        //    }
-        //});
-        var artist = escape(artist);
-        var url = "http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist=" + artist + "&api_key=" + key + "&format=json";
+        var url = "http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist=" + escape(artist) + "&api_key=" + key + "&format=json";
         WinJS.xhr({ url: url, responseType: "json" })
             .done(function complete(result) {
                 var obj = JSON.parse(result.response);
-                var similarArtists = obj.similarartists.artist;
-                callback(similarArtists);
-                similarArtists.forEach(function (artist) {
-                    var photo = artist.image[3]["#text"];
-                    if (photo !== "") {
-                        console.log(photo);
+                var similarArtists = []
+                obj.similarartists.artist.forEach(function (artist) {
+                    for (var i = artist.image.length-1; i >= 0; i--) {
+                        if (artist.image[i]["#text"] != "") {
+                            similarArtists.push({
+                                "img": artist.image[i]["#text"],
+                                "name": artist.name,
+                                "score": artist.match
+                            });
+                            break;
+                        }
                     }
                 });
+                callback(similarArtists);
             }, error);
     };
 
