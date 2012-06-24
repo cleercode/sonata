837fb12622bc2b5666fd8fc34eb1e52d196822a6
Benson Tsai <btsai@vrwarp.com>
2012-06-23 22:27:19 -0700

    youtube.js

---
+++ Ardo/js/youtube.js
@@ -0,0 +1,29 @@
+﻿(function () {
+    "use strict";
+
+    var baseUrl = "https://gdata.youtube.com/feeds/api/videos?v=2&alt=jsonc&category=Music&uploader=partner&q=";
+    var getSongs = function (artist, callback, error) {
+        WinJS.xhr({ url: baseUrl+escape(artist), responseType: "blob" }).done(
+        function (request) {
+            var obj = JSON.parse(request.response);
+            if(!obj.items || obj.items.length == 0) {
+                error();
+                return;
+            }
+
+            var song = obj.items[0];
+            var songData = {
+                "url": song.player.default,
+                "thumbnail": (song.thumbnail.hqDefault ?
+                    song.thumbnail.hqDefault : song.thumbnail.sqDefault),
+                "title": song.title
+            };
+            callback(songData);
+        }, error);
+
+    }
+
+    WinJS.Namespace.define("Youtube", {
+        getSongs: getSongs,
+    });
+})();
